67d5f5770e6a1ef521535617dd795da69307ade3
Blundell <blundell.app.dev@gmail.com>
2013-10-02 13:17:20 +0100

    Updating to step 3 changes

--- src/com/blundell/twitter/MainActivity.java
+++ src/com/blundell/twitter/MainActivity.java
@@ -6,12 +6,15 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity implements GetTweetsTask.OnGotTweetsListener {
 
+    private static final String STATE_TWEETS = "Tweets";
     private ListView listView;
     private View emptyView;
+    private List<Tweet> tweets;
 
     /**
      * Called when the activity is first created.
@@ -21,15 +24,26 @@ public class MainActivity extends Activity implements GetTweetsTask.OnGotTweetsL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new GetTweetsTask(this).execute("Brighton Java");
-
         listView = (ListView) findViewById(R.id.main_list_view);
         emptyView = findViewById(android.R.id.empty);
+
+        if (savedInstanceState == null) {
+            new GetTweetsTask(this).execute("Brighton Java");
+        } else {
+            onGotTweets(savedInstanceState.<Tweet>getParcelableArrayList("Tweets"));
+        }
     }
 
     @Override
     public void onGotTweets(List<Tweet> tweets) {
+        this.tweets = tweets;
         listView.setAdapter(new ArrayAdapter<Tweet>(this, android.R.layout.simple_list_item_1, android.R.id.text1, tweets));
         emptyView.setVisibility(View.GONE);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelableArrayList(STATE_TWEETS, (ArrayList<Tweet>) tweets);
     }
 }
