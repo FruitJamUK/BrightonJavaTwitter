5da0eed464d07ea18c35ca4c8cfbfbefcb735785
Blundell <blundell.app.dev@gmail.com>
2013-10-02 11:53:43 +0100

    new domain object

--- src/com/blundell/twitter/Tweet.java
+++ src/com/blundell/twitter/Tweet.java
@@ -1,6 +1,24 @@
 package com.blundell.twitter;
 
 public class Tweet {
-    String username;
-    String content;
+    private final String username;
+    private final String content;
+
+    public Tweet(String username, String content) {
+        this.username = username;
+        this.content = content;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public String toString() {
+        return username + " : " + content;
+    }
 }
