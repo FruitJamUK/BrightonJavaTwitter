96d2892444474967145d13b07425140291e4e367
Blundell <blundell.app.dev@gmail.com>
2013-11-06 16:20:52 +0000

    implements parcelable

--- src/com/blundell/twitter/Tweet.java
+++ src/com/blundell/twitter/Tweet.java
@@ -1,6 +1,9 @@
 package com.blundell.twitter;
 
-public class Tweet {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Tweet implements Parcelable {
     private final String username;
     private final String content;
 
@@ -22,4 +25,34 @@ public class Tweet {
         return username + " : " + content;
     }
 
+    // Parceleable
+
+    public Tweet(Parcel in) {
+        String[] data = new String[2];
+        in.readStringArray(data);
+        this.username = data[0];
+        this.content = data[1];
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringArray(new String[]{this.username, this.content});
+    }
+
+    public static final Parcelable.Creator<Tweet> CREATOR = new Parcelable.Creator<Tweet>() {
+        @Override
+        public Tweet createFromParcel(Parcel source) {
+            return new Tweet(source);
+        }
+
+        @Override
+        public Tweet[] newArray(int size) {
+            return new Tweet[size];
+        }
+    };
 }
